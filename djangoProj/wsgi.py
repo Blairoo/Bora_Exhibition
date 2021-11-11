3fe20b68629ec1108ba346a4a9c35a656ca80d41
Blairoo <odhlmj@naver.com>
2021-11-11 12:59:26 +0000

    AWS clone

--- djangoProj/wsgi.py
+++ djangoProj/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for exhibition project.
+WSGI config for djangoProj project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exhibition.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProj.settings')
 
 application = get_wsgi_application()
